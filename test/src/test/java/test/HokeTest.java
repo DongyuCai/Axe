1c6443377805e86ced9e583dda91d20f12af20cd
1157656909@qq.com <1157656909@qq.com>
2016-06-07 21:59:08 +0800

    整理Test

--- test/src/test/java/test/HokeTest.java
+++ test/src/test/java/test/HokeTest.java
@@ -23,12 +23,17 @@ public class HokeTest {
 	
 	public static void testHoke(){
 		HelperLoader.init();
-		
-		BannerService bean = BeanHelper.getBean(BannerService.class);
-		long now = System.currentTimeMillis();
 		for(int i=0;i<100;i++){
-			bean.page(1);
+			new Thread("thread-"+i){
+				public void run() {
+					BannerService bean = BeanHelper.getBean(BannerService.class);
+					Page<Banner> page = bean.page(1);
+					System.out.println(this.getName());
+					for(Banner banner:page.getRecords()){
+						System.out.println(banner.getTitle());
+					}
+				};
+			}.start();
 		}
-		System.out.println(System.currentTimeMillis()-now);
 	}
 }
