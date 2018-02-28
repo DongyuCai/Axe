2e6acb23460e357b11fbb0114bdd6dc7bc202afc
DongyuCai <1157656909@qq.com>
2018-02-28 09:14:53 +0800

    MIT 协议调整

--- axe/src/main/java/org/axe/util/JsonUtil.java
+++ axe/src/main/java/org/axe/util/JsonUtil.java
@@ -1,7 +1,7 @@
 /**
  * MIT License
  * 
- * Copyright (c) 2017 The Axe Project
+ * Copyright (c) 2017 CaiDongyu
  * 
  * Permission is hereby granted, free of charge, to any person obtaining a copy
  * of this software and associated documentation files (the "Software"), to deal
