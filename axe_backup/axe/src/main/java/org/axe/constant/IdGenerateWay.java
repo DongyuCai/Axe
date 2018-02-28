e0f413bdb49efa9d754679d43de5302d4966620d
DongyuCai <1157656909@qq.com>
2018-02-28 09:17:30 +0800

    删除备份

--- axe_backup/axe/src/main/java/org/axe/constant/IdGenerateWay.java
+++
@@ -1,9 +0,0 @@
-package org.axe.constant;
-
-/**
- * 表主键的生成方式
- */
-public enum IdGenerateWay {
-	NONE,
-	AUTO_INCREMENT;
-}
--- axe_backup/axe/src/main/java/org/axe/interface_/persistence/BaseTypeConvert.java
+++
@@ -1,9 +0,0 @@
-package org.axe.interface_.persistence;
-
-/**
- * 字段类型转换器
- */
-public  interface BaseTypeConvert  {
-
-	public Object convert(Object object);
-}
